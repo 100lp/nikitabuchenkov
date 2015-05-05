309c42588afdb2efe1bf6ed54bd7b41f4da7c769
100lp <spb.gurme@gmail.com>
2015-05-06 01:29:24 +0300

    set sass, jade and precompile::get ReferceError: reload is not defined

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require("gulp"),
-		connect = require("gulp-connect"),
-		opn = require("opn");
+    connect = require("gulp-connect"),
+    sass = require('gulp-sass'),
+    rename = require("gulp-rename"),
+    autoprefixer = require('gulp-autoprefixer'),
+    jade = require('gulp-jade'),
+    prettify = require('gulp-prettify')
+    opn = require("opn");
 
 // Запускаем локальный сервер
 gulp.task('connect', function() {
@@ -12,17 +17,31 @@ gulp.task('connect', function() {
   opn('http://localhost:8888');
 });
 
-// Работа с HTML
-gulp.task('html', function () {
-  gulp.src('./app/*.html')
-    .pipe(connect.reload());
+
+// Компилируем Jade в html
+gulp.task('jade', function() {
+  gulp.src('app/templates/pages/*.jade')
+    .pipe(jade())
+    .on('error', log)
+    .pipe(prettify({indent_size: 2}))
+    .pipe(gulp.dest('app/'))
+    .pipe(connect.reload())
+    .pipe(reload({stream: true}));
 });
+
 
 // Работа с CSS
 gulp.task('css', function () {
-  gulp.src('./app/css/*.css')
+  gulp.src('./app/sass/*.sass')
+    .pipe(sass({indentedSyntax: true}))
+    .pipe(autoprefixer({
+      browsers: ['last 15 versions', 'ie 8', 'ie 9']
+    }))
+    .pipe(rename('bundle.css'))
+    .pipe(gulp.dest('./app/css'))
     .pipe(connect.reload());
 });
+
 
 // Работа с JS
 gulp.task('js', function () {
@@ -30,12 +49,28 @@ gulp.task('js', function () {
     .pipe(connect.reload());
 });
 
+
 // Слежка
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/**/*.jade'], ['jade']);
   gulp.watch(['./app/js/*.js'], ['js']);
-  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/sass/*.sass'], ['css']);
 });
+
 
 // Задача по-умолчанию
 gulp.task('default', ['connect', 'watch']);
+
+
+// Более наглядный вывод ошибок
+var log = function (error) {
+  console.log([
+    '',
+    "----------ERROR MESSAGE START----------",
+    ("[" + error.name + " in " + error.plugin + "]"),
+    error.message,
+    "----------ERROR MESSAGE END----------",
+    ''
+  ].join('\n'));
+  this.end();
+}
