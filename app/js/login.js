7463c07a70035f9182b4af789318ff6c95a3c6b7
100lp <spb.gurme@gmail.com>
2015-05-15 12:28:51 +0300

    contact-me form

---
+++ app/js/login.js
@@ -0,0 +1,66 @@
+// Модуль авторизации
+var loginModule = (function (){
+
+  var init = function(){
+        console.log('Инициализация модуля loginModule');
+        _setUpListners();
+      },
+
+      _setUpListners = function () {
+        $('#login-form').on('submit', _submitForm);
+      },
+      _submitForm = function (e) {
+        console.log('Сабмит формы');
+
+        e.preventDefault();
+
+        var form = $(this),
+            url = '/login.php',
+            defObject = _ajaxForm(form, url);
+
+        if (defObject) {
+            defObject.done(function(answer) {
+              var message = answer.message,
+                  status = answer.status;
+
+              if (status === 'OK'){
+                form.trigger('reset');
+                form.find('.success-message').text(message).show();
+              } else {
+                form.find('.error-message').text(message).show();
+              }
+
+            });
+        }
+
+      },
+      _ajaxForm = function (form, url) {
+
+        // вовзращает false, если не проходит валидация
+        if (!validation.validateForm(form)) return false;
+
+        var data = form.serialize();
+
+        return $.ajax({
+
+          type: 'POST',
+          url: url,
+          dataType: 'JSON',
+          data: data
+
+        }).fail( function(answer) {
+            console.log('Проблемы в PHP');
+            form.find('.error-message').text('На сервере произошла ошибка').show();
+        });
+
+
+      };
+
+
+  return {
+    init: init,
+  };
+
+})();
+
+loginModule.init();
--- app/js/validation.js
+++ app/js/validation.js
@@ -2,12 +2,12 @@
 var validation = (function (){
 
   var init = function(){
-        console.log('Инициализация модуля validation');
         _setUpListners();
       },
-      validateForm = function (form) { // Проверяет, чтобы все поля формы были не пустыми. Если пустые - вызывает тултипы
-        console.log('Проверяем форму');
 
+      // Проверяет, чтобы все поля формы были не пустыми.
+      // Если пустые - вызывает тултипы
+      validateForm = function (form) {
         var elements = form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
             valid = true;
 
@@ -26,27 +26,30 @@ var validation = (function (){
 
         return valid;
       },
-      _setUpListners = function () { // Прослушивает все события
-        $('form').on('keydown', '.has-error', _removeError); // удаляем красную обводку у элементов форм
-        $('form').on('reset', _clearForm); // при сбросе формы удаляем также: тултипы, обводку, сообщение от сервера
+      // Прослушивает все события
+      _setUpListners = function () {
+        // удаляем красную обводку у элементов форм
+        $('form').on('keydown', '.has-error', _removeError);
+        // при сбросе формы удаляем также: тултипы, обводку, сообщение от сервера
+        $('form').on('reset', _clearForm);
       },
-      _removeError = function() { // Убирает красную обводку у элементов форм
-        console.log('Красная обводка у элементов форм удалена');
-
+      // Убирает красную обводку у элементов форм
+      _removeError = function() {
         $(this).removeClass('has-error');
       },
-      _clearForm = function(form) { // Очищает форму
-        console.log('Очищаем форму');
-
+      // Очищает форму
+      _clearForm = function(form) {
         var form = $(this);
-            form.find('.input, .textarea').trigger('hideTooltip'); // удаляем тултипы
-            form.find('.has-error').removeClass('has-error'); // удаляем красную подсветку
-            form.find('.error-mes, success-mes').text('').hide(); // очищаем и прячем сообщения с сервера
+            // удаляем тултипы
+            form.find('.input, .textarea').trigger('hideTooltip');
+            // удаляем красную подсветку
+            form.find('.has-error').removeClass('has-error');
+            // очищаем и прячем сообщения с сервера
+            form.find('.error-mes, success-mes').text('').hide();
       },
-      _createQtip = function (element, position) { // Создаёт тултипы
-        console.log('Создаем тултип');
-
-        // позиция тултипа
+      // Создание тултипов
+      _createQtip = function (element, position) {
+        // позиция
         if (position === 'right') {
           position = {
             my: 'left center',
@@ -61,8 +64,7 @@ var validation = (function (){
             }
           }
         }
-
-        // инициализация тултипа
+        // инициализация
         element.qtip({
           content: {
             text: function() {
